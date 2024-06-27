86cb16b85c315e24d7bcf695779ba3b4127443a6
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-06-27 21:37:51 +0900

    Feat: JSON 객체로 보내기

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hello node!");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.get("/mango", (req, res) => {
+  res.json({ mango: "my name is MANG!" });
+});
