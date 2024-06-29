785027c9f456724a3544e7fb52620697faa678ea
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-06-29 23:35:04 +0900

    Feat:조건문에 따른 response생성

--- index.js
+++ index.js
@@ -13,3 +13,20 @@ app.listen(port, () => {
 app.get("/mango", (req, res) => {
   res.json({ mango: "my name is MANG!" });
 });
+//params를 이용한 GET
+app.get("/mango/:id", (req, res) => {
+  const mangolingo = req.params.id;
+  console.log(mangolingo);
+  res.send(mangolingo);
+});
+
+app.get("/dogyeong/:name", (req, res) => {
+  const { name } = req.params;
+
+  if (name === "dog") {
+    console.log("mung");
+  } else if (name === "cat") {
+    console.log("nang");
+  }
+  res.json({ myname: name });
+});
