fb6df4cdbd54fae9e91c88edc5641af580166160
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-12-26 23:19:26 +0900

    Feat: dropdown 초안

--- client/src/components/Dropdown.jsx
+++ client/src/components/Dropdown.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import "../scss/Dropdown.scss";
 export const Dropdown = () => {
-  const [drop, setDrop] = useState(true);
+  const [drop, setDrop] = useState(false);
+  const [select, setSelect] = useState(false);
 
-  const place = ["🏠 학교", "💼 직장", "🏫 집", "👟 야외"];
+  const place = ["🏠 학교", "💼 직장", "🏫 가정", "👟 야외"];
 
   const handleDrop = () => {
     setDrop(!drop);
+  };
+
+  const handleSelect = (contents) => {
+    setSelect(contents);
+    console.log(select);
   };
   return (
     <div className="drop-container">
@@ -14,15 +20,25 @@ export const Dropdown = () => {
         className={`dropdown-content ${drop ? "open" : "close"}`}
         onClick={handleDrop}
       >
-        {drop
-          ? place.map((el, idx) => {
-              return (
-                <div key={idx} className="drop-container__list">
-                  {el}
-                </div>
-              );
-            })
-          : "장소"}
+        {drop ? (
+          place.map((el, idx) => {
+            return (
+              <div
+                key={idx}
+                onClick={() => handleSelect(el)}
+                className="drop-container__list"
+              >
+                {el}
+              </div>
+            );
+          })
+        ) : select ? (
+          <div className={`dropdown-content ${select ? "select" : "unselect"}`}>
+            {select}
+          </div>
+        ) : (
+          <div className="dropdown-content__default ">장소</div>
+        )}
       </div>
     </div>
   );
