76e4cf35cf06a78890018f09f3e227eea68b73f2
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2025-01-05 00:43:30 +0900

    WIP : calendar checkTodo 작업중

--- client/src/components/Calendars.tsx
+++ client/src/components/Calendars.tsx
@@ -7,26 +7,67 @@ type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 export const Calendars = () => {
   const [value, onChange] = useState<Value>(new Date());
+  const [monthList, setMonthList] = useState<number[]>([]);
+  const [dayList, setDayList] = useState<number[] | undefined>([]);
+  const [list, setList] = useState({});
   // const [month, setMonth] = useState<number>(0);
   // const [date, setDate] = useState<number>(0);
 
+  const months = [
+    { name: "Jan", value: 0 },
+    { name: "Feb", value: 1 },
+    { name: "Mar", value: 2 },
+    { name: "Apr", value: 3 },
+    { name: "May", value: 4 },
+    { name: "Jun", value: 5 },
+    { name: "Jul", value: 6 },
+    { name: "Aug", value: 7 },
+    { name: "Sep", value: 8 },
+    { name: "Oct", value: 9 },
+    { name: "Nov", value: 10 },
+    { name: "Dec", value: 11 },
+  ];
+
   const newDate = new Date();
   const newDateSplice = new Date().toString().substring(0, 15);
-  const year = newDate.getFullYear();
-  const month = newDate.getMonth() + 1;
-
-  const day = newDate.getDate();
-  const date = `${year}-${month}-${day}`;
-
-  const getAllList = () => {
-    const rawData = localStorage.getItem(date);
-    const list = rawData ? JSON.parse(rawData) : null;
-    console.log(list);
+  //ex) Jan
+  const month = new Date().toString().substring(4, 7);
+  //ex) 2
+  const day = new Date().toString().substring(8, 10);
+  //달을 변환해주고 Number로 바꿈
+  const convertMonth = (month: string) => {
+    for (let i = 0; i < months.length; i++) {
+      if (months[i].name === month) {
+        month = String(months[i].value);
+      }
+    }
+    return Number(month);
   };
 
-  useState(() => {
+  const convertDay = (day: string) => {
+    const dayNumber = Number(day);
+    return dayNumber >= 1 ? dayNumber : -1;
+  };
+
+  const getAllList = () => {
+    const newList1 = [];
+    const newList2 = [];
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = String(localStorage.key(i));
+      const month = key.toString().substring(4, 7);
+      const day = key.toString().substring(8, 10);
+      newList1.push(convertMonth(month));
+      newList2.push(convertDay(day));
+    }
+
+    setMonthList(newList1);
+    setDayList(newList2);
+  };
+
+  useEffect(() => {
     getAllList();
-  });
+    console.log(monthList, dayList);
+  }, []);
 
   return (
     <div
@@ -41,8 +82,18 @@ export const Calendars = () => {
         onChange={onChange}
         value={value}
         onClickDay={(value, event) => alert(value)}
-        tileClassName={({ activeStartDate, date, view }) =>
-          date.getMonth() === 0 && date.getDate() === 2 ? "todo" : null
+        tileClassName={({ date }) =>
+          ["2024-01-01", "2024-01-02", "2024-01-07"].some((todoDate) => {
+            if (
+              new Date(todoDate).getMonth() === date.getMonth() &&
+              new Date(todoDate).getDate() === date.getDate()
+            ) {
+              return true;
+            }
+            return false;
+          })
+            ? "todo"
+            : null
         }
       />
     </div>
