1432b4d290a7d7d2aa7f3ee8deb92f49a879e00f
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2025-01-19 22:55:17 +0900

    WIP: Card Layout 디자인 수정 진행중

--- client/src/components/shopping/Card.tsx
+++ client/src/components/shopping/Card.tsx
@@ -6,13 +6,37 @@ import Typography from "@mui/material/Typography";
 import CardActionArea from "@mui/material/CardActionArea";
 import { ShopAxios } from "@/axios/axios";
 import { useState, useEffect } from "react";
+import "./Card.scss";
+import { Height } from "@mui/icons-material";
+interface ItemType {
+  category: string;
+  description: string;
+  id: string;
+  image: string;
+  name: string;
+  options: Array<{ size: string; price: number }>;
+  price: number;
+  rating: number;
+  review: Array<{ user: string; rating: number; comment: string }>;
+  stock: string;
+}
 
 export default function ActionAreaCard() {
-  const [title, setTitle] = useState("");
-
+  const [electronic, setElectronic] = useState<ItemType[]>([]);
+  const [food, setFood] = useState<ItemType[]>([]);
+  const [pets, setPets] = useState<ItemType[]>([]);
+  const [combination, setCombination] = useState<ItemType[]>([]);
   const mango = async () => {
-    const res = (await ShopAxios.get("/electronics")).data;
+    const res = (await ShopAxios.get("/data")).data;
     console.log(res);
+    const electronic = res.electronics;
+    const food = res.food;
+    const pets = res.pets;
+    const newList = [...electronic, ...food, ...pets];
+    setElectronic(electronic);
+    setFood(food);
+    setPets(pets);
+    setCombination(newList);
   };
 
   useEffect(() => {
@@ -20,24 +44,24 @@ export default function ActionAreaCard() {
   }, []);
 
   return (
-    <Card sx={{ maxWidth: 245 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image="/chi.jpeg"
-          alt="chiiiie"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {title}
-          </Typography>
-          <Typography variant="body2" sx={{ color: "text.secondary" }}>
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <div className="card">
+      {combination.map((el: ItemType) => (
+        <div className="card_container" key={el.id}>
+          <Card>
+            <CardActionArea>
+              <CardMedia component="img" image="/chi.jpeg" alt="chiiiie" />
+              <CardContent style={{ height: 100 }}>
+                <Typography gutterBottom variant="h5" component="div">
+                  {el.name}
+                </Typography>
+                <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                  {el.description}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </div>
+      ))}
+    </div>
   );
 }
