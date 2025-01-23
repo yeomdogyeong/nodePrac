f6d7abc19b374cd2f0e6e37933f6b5582b522bd4
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2025-01-23 21:47:39 +0900

    Feat: DetailPage data 추가

--- client/src/components/shopping/DetailPage.tsx
+++ client/src/components/shopping/DetailPage.tsx
@@ -3,21 +3,55 @@ import { useNavigate, useSearchParams } from "react-router";
 import chieka from "../../assets/chi1.jpeg";
 import "./DetailPage.scss";
 import { ShopAxios } from "@/axios/axios";
+interface ItemType {
+  category: string;
+  description: string;
+  id: string;
+  image: string;
+  name: string;
+  options: Array<{ size: string; price: number }>;
+  price: number;
+  rating: number;
+  review: Array<{
+    user: string;
+    rating: number;
+    comment: string;
+    replies: Array<{ user: string; comment: string }>;
+  }>;
+  stock: string;
+}
+
 export const DetailPage = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const [electronics, setElectronics] = useState<ItemType[]>([]);
+  const [list, setList] = useState<ItemType[]>([]);
+  const category = searchParams.get("category");
   const item = searchParams.get("item");
   const handleData = async () => {
     const res = await (await ShopAxios("/data")).data;
-    console.log(res);
+    const { electronics, food, pets } = res;
+    setElectronics(electronics);
+    console.log(electronics);
   };
 
   useEffect(() => {
     handleData();
-    console.log(item);
   }, []);
+
+  useEffect(() => {
+    console.log(item);
+    console.log(category);
+    if (category === "Electronics") {
+      setList(electronics);
+    }
+    console.log("list", list);
+  }, [electronics, category]);
   return (
     <div className="detail-container">
       <img src={chieka} />
+      {electronics.map((el) => (
+        <div key={el.id}>{el.name}</div>
+      ))}
       <div>name and rating</div>
       <div>dropdown & description</div>
       <div>댓글들</div>
